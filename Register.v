846ad243247c338dff5ccd78290a737b12485513
TarikSalles <tarik.paiva@ufv.br>
2023-06-03 14:44:57 -0300

    Adicionado algumas mudanças

--- Register.v
+++ Register.v
@@ -1,14 +1,24 @@
 /*Incompleto*/
-module Register(Read_Register1, Read_Register2, Write_Register, Write_Data, Read_Data1, Read_Data2);
-	input [4:0] Read_Register1, Read_Register2, Write_Register;
-	input Write_Data; //Incompleto
-	output Read_Data1, Read_Data2; //Incompleto
+module Register(Clock,Read_Register1, Read_Register2, Write_Register, Write_Data, Read_Data1, Read_Data2);
+	input wire Clock;
+	input wire [4:0] Read_Register1, Read_Register2,Write_Register;
+	input wire [31:0] Write_Data;
+	output reg [31:0] Read_Data1, Read_Data2; //Incompleto
 
-	reg read_register1, read_register2, write_register;
+	//reg read_register1, read_register2, write_register;
 	reg write_data;
+	reg [31:0] Registradores [31:0];
+	always@(posedge Clock)begin
+		if (RegWrite == 1)begin //Se RegWrite for verdadeiro,alterar registrador com os dados a serem escritos
+		Registradores[Write_Register] = WriteData;
+		end
+	end
 
+	always@(negedge Clock) begin 
+		ReadData1 = Register[RS1];
+		ReadData2 = Register[RS2];
 	/*Retornar o resultado das saídas*/
-	
+	end
 
 	initial begin
 		$dumpfile("Register.vcd");
