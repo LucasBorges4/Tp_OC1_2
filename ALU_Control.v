4cbe61f6f9aba366968ad86a0f74ec39fe1c53f3
TarikSalles <tarik.paiva@ufv.br>
2023-06-03 16:41:29 -0300

    Atualizado ALU_Control

--- ALU_Control.v
+++ ALU_Control.v
@@ -1,15 +1,43 @@
-module ALUControl(Operation,Funct_Code,Control);
+module ALUControl(Operation,Funct_Code,Control); //Recebe ALUOP, e o opcode, retorna controle
 	input [1:0] Operation;
-	input [5:0] Funct_Code;
+	input [6:0] Opcode;
 	output reg[3:0] Control;
     
-    always case(Funct_Code)
-		32: Operation<=2; //add	
-		34: Operation <=6; //sub
-		36: Operation <=0; //and
-		37: Operation<=1; //or
-		39: Operation <=12; //nor
-		42: Operation <= 7; //slt
-		default: Operation <= 1; //Ilegal
+    always case(Operation)
+		0: Control <= 2; 
+		1: Control <= 6; 
+		2: 
+		begin
+		case(Opcode)
+			32:
+			begin
+				Control = 2;
+			end
+
+			34:
+			begin
+				Control = 6;
+			end
+			
+			36:
+			begin
+				Control = 0;
+			end
+
+			37:
+			begin
+				Control = 1;
+			end
+			
+		endcase
+		end
+
+			
+			
+			
+		//37: Operation<=1; //or
+		//39: Operation <=12; //nor
+		//42: Operation <= 7; //slt
+		default: Control <= 3; //Ilegal
 	endcase
 endmodule
