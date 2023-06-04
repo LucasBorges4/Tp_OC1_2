3b2fc4af2016dcd37f5689bc79bb597c81ab35b1
TarikSalles <114872359+TarikSalles@users.noreply.github.com>
2023-06-04 07:35:47 -0300

    Update Register.v

--- Register.v
+++ Register.v
@@ -20,8 +20,8 @@ module Register(Clock,Read_Register1, Read_Register2, Write_Register, Write_Data
 	end
 
 	always@(negedge Clock) begin 
-		Read_Data1 = Registradores[Read_Data1];
-		Read_Data2 = Registradores[Read_Data2];
+		Read_Data1 = Registradores[Read_Register1];
+		Read_Data2 = Registradores[Read_Register2];
 	/*Retornar o resultado das saídas*/
 	end
 
