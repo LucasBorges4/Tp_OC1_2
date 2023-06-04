1dcecd16e8afaceee41f2f3abf755c88761a73be
TarikSalles <114872359+TarikSalles@users.noreply.github.com>
2023-06-04 10:21:15 -0300

    Update Add.v

--- Add.v
+++ Add.v
@@ -8,3 +8,18 @@ module Add (
     //assign overflow = (x[31] == y[31]) && (x[31] != y[31]);
 
 endmodule
+
+
+model Add_GPTSolution(Input1,Input2,Soma,Overflow);
+input[31:0] Input1,Input2;
+output [31:0] Soma;
+output reg [1:0] Overflow;
+always @* begin
+Soma = Input1 + Input2;
+Overflow = (Soma > 2**31-1);
+end
+endmodule
+
+    //assign overflow = (x[31] == y[31]) && (x[31] != y[31]);
+
+endmodule
