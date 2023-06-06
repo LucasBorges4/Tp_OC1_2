666a0dab7608ab773439b6227a2f2d38723ca0c9
TarikSalles <tarik.paiva@ufv.br>
2023-06-06 10:11:01 -0300

    Erro Imediato aparentemente

--- Immediate_generation.v
+++ Immediate_generation.v
@@ -1,21 +1,22 @@
-module Old_Immediate_Generation(Input,Output);
-    input [11:0] Input;
-    output reg[31:0] Output;
-    always @(Input) begin
-        reg Bit_Esquerda = Input[11];
+//module Old_Immediate_Generation(Input,Output);
+//    input [11:0] Input;
+ //   output reg[31:0] Output;
+  //  always @(Input) begin
+  //      reg Bit_Esquerda = Input[11];
 
-        if (Bit_Esquerda) begin
-            Output = { {20{1'b1}}, Input[11:0] };
-        end else begin
-            Output = { {20{1'b0}}, Input[11:0] };
-        end
-    end
+    //    if (Bit_Esquerda) begin
+      //      Output = { {20{1'b1}}, Input[11:0] };
+        //end else begin
+         //   Output = { {20{1'b0}}, Input[11:0] };
+      //  end
+    //end
 
-endmodule
+//endmodule
+
 
 module Immediate_Generation(Input,Output);
-    input [11:0] Input,
-    output reg [31:0] Output,
+    input [11:0] Input;
+    output reg [31:0] Output;
     always @(*) begin
         Output = {Input[11], Input[11], Input[11], Input[11], Input[11], Input[11], Input[11], Input[11], Input[11], Input[11], Input[11], Input[11], Input[11], Input[11], Input[11], Input[11], Input[11], Input[11], Input[11], Input[11], Input};
     end
