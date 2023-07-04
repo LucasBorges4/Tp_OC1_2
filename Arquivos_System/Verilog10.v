1fe2dfb3638b46259a5d7fc51a9a8b9a8033bdca
TarikSalles <tarik.paiva@ufv.br>
2023-07-03 21:16:26 -0300

    Commit
    Commit

--- Arquivos_System/Verilog10.v
+++
@@ -1,9 +0,0 @@
-module Add(Input1,Input2,Soma,Clock);
-input[31:0] Input1,Input2;
-input Clock;
-output reg [31:0] Soma;
-always @(*) begin
-Soma <= Input1 + Input2;
-//$display("%b",Soma);
-end
-endmodule
--- feito/MUX.v
+++
@@ -1,7 +0,0 @@
-module MUX(A, B, S, Out);
-    input [31:0] A,B;
-    input S;
-    output [31:0] Out;
-    assign Out = (S == 1'b0) ? A : B;
-endmodule
-
